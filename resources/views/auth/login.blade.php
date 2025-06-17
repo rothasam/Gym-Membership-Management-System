46c6cb4c8935656f8cc13eb8f8d84feb911eb2a4
Rotha Sam <samrotha98@gmail.com>
2025-06-17 21:20:21 +0700

    install boostrap

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,10 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1 class="text-danger">This is testing</h1>
-
+    <h1>login page</h1>
 </body>
 </html>
