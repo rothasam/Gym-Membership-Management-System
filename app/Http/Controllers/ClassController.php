de7700cc927f0602e6e0ad04c5da072ea125f3d5
SivThean <sivtheanv@gmail.com>
2025-06-23 20:02:40 +0700

    completed ClassController

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -9,29 +9,43 @@ class ClassController extends Controller
 {
     public function index() {
 
-        $classes = GymClass::all();
+
+        // $classes = GymClass::all();
+        $classes = GymClass::where('is_deleted', 0)->get();
         return view('classes.index', compact('classes'));
+    }   
+
+    public function add() {
+    return view('classes.add'); 
     }
 
     public function create () {
 
         return view('classes.create');
+        // return view('classes.add');
     }
 
     public function store(Request $request) {
-
+        //  dd($request->all());
+        
         $request->validate([
-           'class_name' => 'required|string|max:255',
+        //    'class_name' => 'required|string|max:255',
+        //     'description' => 'nullable|string',
+        //     'total_member' => 'nullable|integer',
+        //     'start_time' => 'nullable|date',
+        //     'end_time' => 'nullable|date|after:start_time',
+            'class_name' => 'required|string|max:255',
             'description' => 'nullable|string',
             'total_member' => 'nullable|integer',
-            'start_time' => 'nullable|date',
-            'end_time' => 'nullable|date|after:start_time',
+            'start_time' => 'nullable|date_format:H:i',
+            'end_time' => 'nullable|date_format:H:i|after:start_time',
         ]);
 
         GymClass::create($request->all());
 
         return redirect()->route('classes.index')->with('success', 'Class created successfully!');
-    }  
+    }
+  
     
     public function show(GymClass $classes) {
 
@@ -45,24 +59,14 @@ class ClassController extends Controller
 
     public function  update(Request $request, GymClass $classes) {
 
-        $request->validate([
-            'class_name' => 'required|string|max:255',
-            'description' => 'nullable|string',
-            'total_member' => 'nullable|integer',
-            'start_time' => 'nullable|date',
-            'end_time' => 'nullable|date|after:start_time',
-        ]);
-
-        $classes->update($request->all());
-
+        $classes->update($request->all());        
         return redirect()->route('classes.index')->with('success', 'Class updated successfully!');
     }
 
 
     public function destroy(GymClass $classes ) {
 
-    //    $classes->deleted();
-
-        // return redirect()->route('classes.index')->with('success', 'Class deleted successfully!');
+        $classes->update(['is_deleted' => 1]);
+        return redirect()->route('classes.index')->with('success', 'Class moved to trash successfully!');
     }
 }
